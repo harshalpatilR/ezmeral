aa1fd346f929968a0117f060086d751b2551114f
Harshal Patil <harshal-sharadchandra.patil@hpe.com>
2024-10-01 11:55:36 +0800

    update

--- Airflow/python_producer.py
+++ Airflow/python_producer.py
@@ -33,38 +33,50 @@ def run_python_script(script_path):
     subprocess.run(["python", script_path], check=True)
     print(f"Python script {script_path} executed successfully")
 
+dag_venv_path = "/mnt/shared/shared/airflow/pyenv1"
+dag_code_path = "/mnt/shared/shared/airflow-jobs/pytest.py"
+dag_req_path = "/mnt/shared/shared/airflow-jobs/req1.txt"
+
 with DAG(
     dag_id="virtualenv_and_python_script",
     default_args={
-        'owner': 'your_name',
-        'start_date': days_ago(2)
+        "owner": "airflow",
+        "depends_on_past": False,
+        "start_date": days_ago(1),
+        "email": ["airflow@example.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "max_active_runs": 1,
+        "retries": 0,
     },
-    schedule_interval="@daily"
+    schedule_interval=None,
+    tags=["Python", "Kafka", "Producer"],
+    access_control={"All": {"can_read", "can_edit", "can_delete"}},
 ) as dag:
 
     create_venv = PythonOperator(
         task_id="create_venv",
         python_callable=create_virtualenv,
-        op_kwargs={'venv_path': '/path/to/your/virtualenv'}
+        op_kwargs={'venv_path': dag_venv_path}
     )
 
     activate_venv = PythonOperator(
         task_id="activate_venv",
         python_callable=activate_virtualenv,
-        op_kwargs={'venv_path': '/path/to/your/virtualenv'},
+        op_kwargs={'venv_path': dag_venv_path},
         trigger_rule="all_success"
     )
 
     install_packages = PythonOperator(
         task_id="install_packages",
         python_callable=install_packages,
-        op_kwargs={'venv_path': '/path/to/your/virtualenv', 'requirements_file': '/path/to/your/requirements.txt'}
+        op_kwargs={'venv_path': dag_venv_path, 'requirements_file': dag_req_path}
     )
 
     run_script = PythonOperator(
         task_id="run_script",
         python_callable=run_python_script,
-        op_kwargs={'script_path': '/path/to/your/script.py'},
+        op_kwargs={'script_path': dag_code_path},
         trigger_rule="all_success"
     )
 
@@ -77,4 +89,3 @@ with DAG(
 
     create_venv >> install_packages >> activate_venv >> run_script >> deactivate_venv
 
-    
